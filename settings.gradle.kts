c34c695fd50b10b3575cfd0186e1b59c439d3119
AM <am@Mac-Mini-M2.local>
2025-12-15 11:55:56 +0100

    Correction plugin settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,17 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "com.github.recloudstream.cloudstream") {
+                useModule("com.github.recloudstream:gradle:master-SNAPSHOT")
+            }
+        }
+    }
+}
+
 rootProject.name = "AnimeSama-Cloudstream"
 include(":AnimeSama")

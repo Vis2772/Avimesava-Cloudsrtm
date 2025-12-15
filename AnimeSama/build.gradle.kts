5c258589eaeb9f4f5d1bd7600b19d7ea64935740
AM <am@Mac-Mini-M2.local>
2025-12-15 12:32:22 +0100

    Mise a jour standards 2025

--- AnimeSama/build.gradle.kts
+++ AnimeSama/build.gradle.kts
@@ -1,39 +1,33 @@
-plugins {
-    id("com.android.library")
-    id("kotlin-android")
-    id("com.github.recloudstream.cloudstream")
-}
+// use an integer for version numbers
+version = 4
 
-android {
-    compileSdk = 30
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 30
-    }
-    buildFeatures {
-        viewBinding = true
-    }
-}
 
 cloudstream {
-    // Les infos de ton extension
-    description = "Anime-Sama par Vis2772"
-    authors = listOf("Vis2772")
+    // All of these properties are optional, you can safely remove them
+
+    description = "Anime-Sama est un site de référencement et de catalogage, créé par des passionnés de l’animation et du divertissement APAC."
+    authors = listOf("ycngmn")
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
     status = 1
+
     tvTypes = listOf("Anime")
+
     requiresResources = true
     language = "fr"
+
+
     iconUrl = "https://cdn.statically.io/gh/Anime-Sama/IMG/img/autres/logo.png"
 }
 
-// Numéro de version (change le chiffre quand tu fais une mise à jour)
-version = 4
-
-dependencies {
-    // Les outils nécessaires pour Cloudstream
-    val cloudstream3Version = "pre-release"
-    implementation("com.github.recloudstream.cloudstream:library:$cloudstream3Version")
-    implementation("com.github.recloudstream.cloudstream:app:$cloudstream3Version")
-    implementation("org.jsoup:jsoup:1.13.1")
-    implementation(kotlin("stdlib"))
+android {
+    buildFeatures {
+        viewBinding = true
+    }
 }

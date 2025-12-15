e1013a42151b96b9b16f577ac0716185ac8848ef
AM <am@Mac-Mini-M2.local>
2025-12-15 11:40:54 +0100

    Vraie sauvegarde cette fois

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,12 @@ buildscript {
     repositories {
         google()
         mavenCentral()
+        maven("https://jitpack.io") // Ajoute cette ligne
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.4")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT") // Ajoute cette ligne
     }
 }
 
@@ -15,8 +17,4 @@ allprojects {
         mavenCentral()
         maven("https://jitpack.io")
     }
-}
-
-task<Delete>("clean") {
-    delete(rootProject.buildDir)
 }
